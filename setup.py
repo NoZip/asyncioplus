86c45a9abdd558d8b8480b99abdd49956b28ffdd
Thomas Pizon <pizon.thomas@gmail.com>
2015-11-03 16:10:45 +0100

    setup added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name="AsyncioPlus",
+    description="Python asyncio addons",
+    url="https://github.com/NoZip/asyncioplus",
+    packages=["asyncioplus"]
+)
