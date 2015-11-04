f5dd65410b20dcf5ab853dd5726e313dc9bb5fa0
Thomas Pizon <pizon.thomas@gmail.com>
2015-11-04 16:44:48 +0100

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="AsyncioPlus",
-    version="0.1.0"
+    version="0.1.0",
     description="Python asyncio addons",
     url="https://github.com/NoZip/asyncioplus",
     packages=["asyncioplus"]
